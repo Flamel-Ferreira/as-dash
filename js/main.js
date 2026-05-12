c697aaee5b5036991f63e91eb993542509be6614
Flamel Ferreira <flamelferreira05@hotmail.com>
2026-05-12 11:41:07 -0300

    feat: Função de resize;

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const BREAKPOINTS = {
+  mobile: 425,
+  tablet: 768,
+  desktop: 1024,
+  ultrawide: 1440,
+};
+
+/**
+ * Função de Resize: Executa toda vez que a tela muda de tamanho e quando carrega a tela.
+ */
+function onResize() {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  let currentBreakpoint = 'mobile'; // Default
+
+  Object.keys(BREAKPOINTS).forEach(key => {
+    const bpWidth = BREAKPOINTS[key];
+    if (width >= bpWidth) currentBreakpoint = key;
+  });
+
+  document.body.classList.remove(...Object.keys(BREAKPOINTS));
+  document.body.classList.add(currentBreakpoint);
+  
+  console.log(`Breakpoint atual: ${currentBreakpoint}`);
+}
+
+document.addEventListener('DOMContentLoaded', onResize);
+window.addEventListener('resize', onResize);
+
