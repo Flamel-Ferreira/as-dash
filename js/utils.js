f81df8c0fad6d81789c666ec03bb24281e2ff8c3
Flamel Ferreira <flamelferreira05@hotmail.com>
2026-05-12 10:54:44 -0300

    refactor: configurando SVG's
    - Adicionar svg's em um único arquivo;
    - Remoção de svg's não usados;
    - SVG Injection no utils;
    for consistency and optimization.

---
+++ js/utils.js
@@ -0,0 +1,9 @@
+// SVG Injection
+fetch('assets/icons.svg')
+  .then(response => response.text())
+  .then(data => {
+    const div = document.createElement('div');
+    div.style.display = 'none';
+    div.innerHTML = data;
+    document.body.insertBefore(div, document.body.childNodes[0]);
+  });
